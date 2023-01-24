289190e0fae0c44582d5f9c8317d974895626dd1
Quentin Beauperin <quentin.beauperin@gmail.com>
2023-01-24 13:07:28 -0800

    [IMAGES] Add dynamic image generation (resizing/renaming)

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,29 @@
 const CleanCSS = require("clean-css");
+const Image = require('@11ty/eleventy-img');
 const util = require('util');
 
+async function thumbnailShortcode(src, alt, position) {
+    // Make sure alt is provided
+    if (alt === undefined) {
+        throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+    }
+
+    // Generate images
+    let metadata = await Image(src, {
+        widths: [1200, 750],
+        formats: ["webp"],
+        outputDir: '_site/assets/images',
+        urlPath: '/assets/images'
+    });
+
+    let images = metadata.webp;
+    return `<a class="thumbnail" href="${images[1].url}" data-position="${position}"><img src="${images[0].url}" alt="${alt}" /></a>`;
+}
+
 module.exports = function (eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("assets/");
+    eleventyConfig.addPassthroughCopy("assets/css/");
+    eleventyConfig.addPassthroughCopy("assets/js/");
+    eleventyConfig.addPassthroughCopy("assets/svg/");
 
     eleventyConfig.addFilter("cssmin", function (code) {
         return new CleanCSS({}).minify(code).styles;
@@ -11,5 +32,6 @@ module.exports = function (eleventyConfig) {
         const str = util.inspect(value);
         return `<div style="white-space: pre-wrap;">${unescape(str)}</div>;`
     });
-    // eleventyConfig.addWatchTarget("css/");
+
+    eleventyConfig.addAsyncShortcode("thumbnail", thumbnailShortcode);
 };
--- posts/2023-01-22-calming-sunset.md
+++ posts/2023-01-22-calming-sunset.md
@@ -1,5 +1,5 @@
 ---
-title: "Sunset over bridge"
-image: "assets/images/large.png"
+title: "Calming sunset"
+image: "assets/images/piermanuele-sberni-W88UsnaGuog-unsplash.jpg"
 ---
 I took this photos while heading home one day and got strucked with this *magnificent sunset*.
